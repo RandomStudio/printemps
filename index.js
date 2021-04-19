ba1d522e3f2886d10d576903c7d9d8f67ef786af
Stephen Buchanan <stephen.buchanan@random.studio>
2021-04-19 12:14:30 +0100

    print single object result

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const { exec } = require("child_process");
 
+let output = {};
+
 exec("cat /sys/class/thermal/thermal_zone0/temp", (err, stdout, stderr) => {
   const cpuTemp = parseInt(stdout.trim()) / 1000.0;
-  console.log({ cpuTemp });
+  output = { ...output, cpuTemp };
+
+  exec("/opt/vc/bin/vcgencmd measure_temp", (err, stdout, stderr) => {
+    const gpuTemp = parseFloat(stdout.trim().replace("temp=", "").replace("'C", ""));
+    output = { ...output, gpuTemp };
+
+    outputFinalResults(output);
+
+  })
 });
 
-exec("/opt/vc/bin/vcgencmd measure_temp", (err, stdout, stderr) => {
-  const gpuTemp = parseFloat(stdout.trim().replace("temp=", "").replace("'C", ""));
-  console.log({ gpuTemp });
-})
+function outputFinalResults(data) {
+  console.log({ ...data });
+}
