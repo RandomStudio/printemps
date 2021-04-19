3962cdde46446b782e858bb158ec778e51e536fe
Stephen Buchanan <stephen.buchanan@random.studio>
2021-04-19 12:06:47 +0100

    format cpu temp properly

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { exec } = require("child_process");
 
 exec("cat /sys/class/thermal/thermal_zone0/temp", (err, stdout, stderr) => {
-  console.log({stdout});
+  const cpuTemp = parseInt(stdout.trim()) / 1000.0;
+  console.log({ cpuTemp });
 });
+
