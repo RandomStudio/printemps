9a0f0334341b4934cf1154549d23bdd5026c964b
Stephen Buchanan <stephen.buchanan@random.studio>
2021-04-19 12:10:55 +0100

    also get GPU temp

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ exec("cat /sys/class/thermal/thermal_zone0/temp", (err, stdout, stderr) => {
   console.log({ cpuTemp });
 });
 
+exec("/opt/vc/bin/vcgencmd measure_temp", (err, stdout, stderr) => {
+  const gpuTemp = parseFloat(stdout.trim().replace("temp=", "").replace("'C", ""));
+  console.log({ gpuTemp });
+})
